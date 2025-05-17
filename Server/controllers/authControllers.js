8c0dc35a35b091799cf9fc0a065dd7191e6542af
Anees A L <aneesal874@gmail.com>
2025-05-17 09:26:28 +0530

    authetication controllers

---
+++ Server/controllers/authControllers.js
@@ -0,0 +1,102 @@
+import bcrypct from 'bcryptjs'
+import userModel from '../Models/userModel';
+
+
+export const register=async(req,res)=>{
+  const{name,email,password}=req.body;
+
+  if(!name || !email||!password){
+    return res.json({sucess:false,message:'missing details'});
+
+
+  }
+  try{
+         
+       const existingUser=await userModel.findOne({email});
+
+       if(existingUser)
+        {
+
+        return res.json({sucess:false,message:"user already existing"});
+
+        }
+        const hashedpassword=await bcrypct.hash(password,10);
+
+       const user=new userModel({name,email,password:hashedpassword});
+       await user.save();
+
+const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
+
+       res.cookie('token',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV === 'production'? 'none':'strict',
+        maxAge:7*24*60*60*1000
+       });
+
+
+  }catch(error){
+    res.json({sucess:false,message:error.message});
+    
+  }
+}
+
+
+
+export const login=async(req,res)=>{
+  const{email,password}=req.body;
+  if(!email || !password){
+      return res.json({sucess:false,message:"user not existed"});
+  }
+
+  try{
+    const user=   await userModel.findOne({email});
+    if(!user){
+      return res.json({sucess:false,message:"invalid email"});
+    }
+
+    const isMatch= await bcrypct.compare(password,user.password);
+    if(!isMatch){
+      return res.json({sucess:false,message:"invalid password"});
+    }
+
+    const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
+
+       res.cookie('token',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV === 'production'? 'none':'strict',
+        maxAge:7*24*60*60*1000
+       });
+
+
+       return res.json({sucess:true});
+
+
+  }catch(error){
+    res.json({sucess:false,message:error.message});
+  }
+}
+
+
+
+export const logout=async(req,res)=>{
+  try{
+      res.clearCookie('token',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV === 'production'? 'none':'strict',
+        
+       });
+
+       return res.json({sucess:true,message:"logout sucessfully"})
+
+  }catch(error){
+    res.json({sucess:false,message:error.message});
+  }
+}
+
+
+
+
+       
